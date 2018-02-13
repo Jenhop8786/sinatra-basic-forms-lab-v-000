e17d8e4a79d00804554ca46628403ce0d265dc2d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-13 03:08:43 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,19 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/' do
 
+  get '/' do
     erb :index
   end
 
   get '/new' do
-
     erb :create_puppy
   end
 
   post '/' do
-    @new_puppy = Puppy.new(params[:name],params[:age], params[:breed])
-
+    @new_puppy = Puppy.new(params[:name], params[:breed], params[:age])
     erb :display_puppy
   end
 end
